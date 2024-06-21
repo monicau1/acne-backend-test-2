15055db7e3fe4a3262afe474e2ca2f2b4ae630b6
monicau1 <monicaong021@gmail.com>
2024-06-21 13:32:35 +0700

    ganti service account

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.use("/api/images", imagesRoute);
 app.use("/api/acne-detector", acneDetectorRoute); // Gunakan route baru ini
 
 // Server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
